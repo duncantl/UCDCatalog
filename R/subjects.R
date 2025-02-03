ca7d6399f6db7dd8b64392db922de15174d54768
Duncan Temple Lang <duncan@r-project.org>
2025-02-02 16:37:25 -0800

    drop the link to the pdf of the page

--- R/subjects.R
+++ R/subjects.R
@@ -5,9 +5,8 @@ function(u = "https://catalog.ucdavis.edu/courses-subject-code/")
 {
     doc = htmlParse(readLines(u))
     a = getNodeSet(doc, "//a[starts-with(@href, '/courses-subject-code/')]")
-    browser()
-    txt = sapply(a, xmlValue)    
-    w = grepl("\\([A-Z]{3}\\)$", txt)
+    txt = sapply(a, xmlValue)
+    w = grepl("\\([A-Z]{3}\\)$", txt) & !grepl("Download Page", txt)
     txt2 = trimws(gsub("\\([A-Z]{3}\\)$", "", txt[w]))
     names(txt2) = toupper( basename(sapply(a[w], xmlGetAttr, "href")))
     txt2
